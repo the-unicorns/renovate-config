a0accf3679907ee681ba56d2e8345f7ba85ad233
Hans Jakob Emmel <hans.emmel@gmail.com>
2020-09-13 13:47:45 +1200

    fix: change MasterIssue to dependencyDashboard and fix tests
    As per issue [#6729](https://github.com/renovatebot/renovate/pull/6729) renovate changed masterIssue
    configurations to dependencyDashboard causing breaking changes. I fixed the broken keys in
    package.json and related tests
    
    BREAKING CHANGE: Change from masterIssue to depdencyDashboard

--- basic.test.js
+++ basic.test.js
@@ -1,33 +1,35 @@
-const test = require('tape');
-const pkgJson = require('./package.json');
+const test = require("tape");
+const pkgJson = require("./package.json");
 
 // Validate the config with Renovate
-require('renovate/dist/config-validator');
+require("renovate/dist/config-validator");
 
-const renovatePkgExtends = pkgJson['renovate-config'].default.extends;
+const renovatePkgExtends = pkgJson["renovate-config"].default.extends;
 
 const baseRules = [
-    "config:base",
-    ":masterIssue",
-    ":masterIssueApproval",
-    ":labels(renovate, dependencies)",
-    "group:monorepos",
-    "group:recommended",
-    ":semanticCommits"
-]
+  "config:base",
+  ":dependencyDashboard",
+  ":dependencyDashboardApproval",
+  ":labels(renovate, dependencies)",
+  "group:monorepos",
+  "group:recommended",
+  ":semanticCommits",
+];
 
-test('Check if extends is correct', function (t) {
+test("Check if extends is correct", function (t) {
+  t.equal(renovatePkgExtends.length, 7);
 
-    t.equal(renovatePkgExtends.length, 7)
-
-    renovatePkgExtends.forEach((item) => {
-        t.ok(baseRules.includes(item), `Rule ${item} is present`);
-    });
-    t.end();
+  renovatePkgExtends.forEach((item) => {
+    t.ok(baseRules.includes(item), `Rule ${item} is present`);
+  });
+  t.end();
 });
 
-test('Check master issue auto close', function (t) {
-    t.ok(pkgJson['renovate-config'].default.masterIssueAutoclose, `Master issue auto close enabled`);
+test("Check master issue auto close", function (t) {
+  t.ok(
+    pkgJson["renovate-config"].default.dependencyDashboardAutoclose,
+    `Master issue auto close enabled`
+  );
 
-    t.end();
+  t.end();
 });
